77be2a3f161ca533ebe9bb21996f9c7c8cdaeaf0
Matheus S Souza <andzedd.matheus@gmail.com>
2020-07-22 20:22:56 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let theme = localStorage.getItem("theme");
+
+if(theme == null){
+    setTheme('light');
+} else {
+    setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for(var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode;
+        setTheme(mode);
+    })
+}
+
+function setTheme(mode){
+    if(mode == "light"){
+        document.getElementById('theme-style').href = 'default.css';
+    }
+
+    if(mode == "nord"){
+        document.getElementById('theme-style').href = 'nord.css';
+    }
+
+    localStorage.setItem('theme', mode);
+}
