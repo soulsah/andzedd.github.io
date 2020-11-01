6607511023fe877724a3d9788e67c075a98b979f
Matheus S Souza <andzedd.matheus@gmail.com>
2020-11-01 00:34:54 -0300

    added profile avatar mouse animation

--- js/script.js
+++ js/script.js
@@ -55,3 +55,21 @@ shuffle.forEach(element => {
         element.innerText = originalText
     })
 });
+
+const card = document.querySelector(".card");
+const web = document.querySelector(".intro-wrapper");
+
+web.addEventListener("mousemove", e => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+web.addEventListener("mouseenter", e => {
+    card.style.transition = 'none';
+})
+
+web.addEventListener("mouseleave", e => {
+    card.style.transition = 'all 0.5s ease';
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+})
